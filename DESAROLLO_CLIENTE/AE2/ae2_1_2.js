12756b6d4fe87ff0dcfcebbb3f77862e8cb4a8e3
Diego Chilet <diegochilet99@gmail.com>
2020-10-01 09:03:36 +0200

    subiendo ae2_1_2

---
+++ DESAROLLO_CLIENTE/AE2/ae2_1_2.js
@@ -0,0 +1,9 @@
+
+
+    function par(x){
+        return x%2 == 0;
+        }
+        let values=["Ana", 2, "Javi", 5, "Roberto", 10];
+        let texto = values.filter(par);
+        console.log(texto);
+
