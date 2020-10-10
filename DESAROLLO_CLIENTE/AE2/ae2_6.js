69ebb9c86df2b599ecb8be322721fad8a9d64e25
Diego Chilet <diegochilet99@gmail.com>
2020-10-11 01:48:07 +0200

    añadiendo 6

---
+++ DESAROLLO_CLIENTE/AE2/ae2_6.js
@@ -0,0 +1,36 @@
+/* function ae2(nombres) { */
+ 
+/*  users = ["diego", "sento", "carlos", "ximo"];
+blacklisted = ["ximo"];  */
+
+/* 
+    nombres.surname = "Doe";
+    }
+    let array = [{"name":"Pepe"},{"name":"Juan"}];
+    array.forEach(ae2);
+    console.log(array[0]);
+    console.log(array[1]);  */
+
+
+/*     function nombres(x){
+        return x%2 == 0;
+        }
+        users = ["diego", "sento", "carlos", "ximo"];
+        blacklisted = ["ximo"]; 
+        console.log( array.some(nombres) ); */
+
+
+  /*       function par(x){
+            return x%2 == 0;
+            }
+            let array = [1,2,3,4,5];
+            let newArray = array.filter(par);
+            console.log(newArray);
+ */
+function ae2(){
+        return blacklisted;
+    }
+    let users = ["diego", "sento", "carlos", "ximo"];
+    let blacklisted = ["ximo"]; 
+    let nombresDef = users.filter(ae2);
+    console.log(nombresDef);
