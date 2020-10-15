bfdbd226f6a69e4319e65616e870ae30321c241e
Diego Chilet <diegochilet99@gmail.com>
2020-10-15 08:51:16 +0200

    añadiendo y cambiando archivos

---
+++ DESAROLLO_CLIENTE/ANE_DOM/index.js
@@ -0,0 +1,7 @@
+function cambiarImagenJS(){
+    if(document.getElementById("logo").src=="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSWhikotYVmDgwqJFXMLTfc9PE6JF5mXMjBGQ&usqp=CAU"){
+        document.getElementById("logo").src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT3mhxgIRaZ9NhGxPdNIv24l6bQ-kjyAKbT9Q&usqp=CAU"
+    }else{
+        document.getElementById("logo").src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSWhikotYVmDgwqJFXMLTfc9PE6JF5mXMjBGQ&usqp=CAU";
+    }
+   }
