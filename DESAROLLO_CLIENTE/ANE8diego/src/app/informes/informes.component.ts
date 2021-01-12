a7cb78e516c7742af7db7105fd89e8b78f8d1376
Diego Chilet <diegochilet99@gmail.com>
2021-01-12 19:38:37 +0100

    tabla acabada

--- DESAROLLO_CLIENTE/ANE8diego/src/app/informes/informes.component.ts
+++ DESAROLLO_CLIENTE/ANE8diego/src/app/informes/informes.component.ts
@@ -1,19 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { Product } from '../../domain/product';
-import { ProductService } from '../../service/productservice';
+
 
 @Component({
   selector: 'app-informes',
-  templateUrl: '../tablebasicdemo.html',
+  templateUrl: './informes.component.html',
   styleUrls: ['./informes.component.css'],
 })
-export class TableBasicDemo implements OnInit {
+export class InformesComponent implements OnInit {
 
-  productos: Product[];
 
-  constructor(private productService: ProductService) { }
+  constructor() { }
 
   ngOnInit() {
-      this.productService.getProductsSmall().then(data => this.productos = data);
   }
 }
--- DESAROLLO_CLIENTE/ANE8diego/src/app/productos/productos.component.ts
+++ DESAROLLO_CLIENTE/ANE8diego/src/app/productos/productos.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-productos',
-  templateUrl: './productos.component.html',
-  styleUrls: ['./productos.component.css']
+    selector: 'app-productos',
+    templateUrl: './productos.component.html',
+    styleUrls: ['./productos.component.css']
 })
 export class ProductosComponent implements OnInit {
 
-  constructor() { }
+    usuarios=<any>[];
 
-  ngOnInit(): void {
-  }
+    constructor(private httpClient: HttpClient) { }
 
+    ngOnInit(): void{
+        this.httpClient.get("https://jsonplaceholder.typicode.com/users").subscribe(
+          response => {
+            this.usuarios=response;
+            console.log(this.usuarios);
+          }
+        )
+    }
 }
