0e2f1c92e66948cad751d252ebaf3cf4e3e472ba
Diego Chilet <diegochilet99@gmail.com>
2021-02-05 10:48:15 +0100

    haciendo el ej3 y intentando el ej2.

--- DESAROLLO_CLIENTE/EXAMEN 2ª EVALUACIÓN - DIEGO/examen2ev/src/app/app-routing.module.ts
+++ DESAROLLO_CLIENTE/EXAMEN 2ª EVALUACIÓN - DIEGO/examen2ev/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EXSecondViewComponent } from 'src/exsecond-view/exsecond-view.component';
 import { EXButtonComponent } from '../exbutton/exbutton.component';
 
 const routes: Routes = [
   {path: 'boton', component: EXButtonComponent},
+  {path: 'boton2', component: EXSecondViewComponent},
 ];
 
 @NgModule({
